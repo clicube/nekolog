7f62d19b1430fee41ef163adb80b5ca2ed2ec376
clicube <clicube@gmail.com>
2018-05-20 19:13:55 +0900

    wip

--- events.js
+++ events.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const TableName = "Events"
+const TableName = "Events";
 
 const AWS = require('aws-sdk');
 const ddc = new AWS.DynamoDB.DocumentClient();
 
 module.exports.get = (callback) => {
-    ddc.get({
+    return ddc.get({
         TableName: TableName
     }).promise()
     .then( (result) => { callback(result) } )
-}
+};
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const serverless = require('serverless-http');
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-const events = require('./events.js')
+const events = require('./events.js');
 
 app.get('/events', (req, res, next) => {
   events.get().promise()
-  .then( (result) => { res.send(result) } )
+  .then( (result) => { res.send(result) } );
 });
 
 module.exports.handler = serverless(app);
