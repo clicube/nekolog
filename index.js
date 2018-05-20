f2d7367805c649fc6e51edc2d96dc27e1f7df676
clicube <clicube@gmail.com>
2018-05-20 16:03:18 +0900

    wip

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const serverless = require('serverless-http');
 const express = require('express')
 const app = express()
 
-app.get('/events', function (req, res) {
-  res.send('Hello World!')
-})
+const AWS = require('aws-sdk');
+const ddc = new AWS.DynamoDB.DocumentClient();
+
+app.get('/events', (req, res, next) => {
+  ddc.scan({
+    TableName: "eventsTable"
+  }).promise()
+  .then((result) => res.send(result))
+  .catch(next)
+});
 
 module.exports.handler = serverless(app);
