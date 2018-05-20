94f6e6b80455e070cd7cb62a44c6248fc20c1db7
clicube <clicube@gmail.com>
2018-05-20 19:01:50 +0900

    wip

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const serverless = require('serverless-http');
 const express = require('express')
 const app = express()
 
-const events = require('events')
-
+const events = require('./events.js')
 
 app.get('/events', (req, res, next) => {
   events.get().promise()
-  .then((result) => {res.send(result)})
+  .then( (result) => { res.send(result) } )
 });
 
 module.exports.handler = serverless(app);
