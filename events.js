68a10e6ff38418a28bb7a088db853c39077b5f90
clicube <clicube@gmail.com>
2018-05-20 19:27:56 +0900

    wip

--- events.js
+++ events.js
@@ -6,11 +6,11 @@ const AWS = require('aws-sdk');
 const ddc = new AWS.DynamoDB.DocumentClient();
 
 function validate(event) {
-    
+
 }
 
 module.exports.get = () => {
-    return ddc.get({
+    return ddc.scan({
         TableName: TableName
     }).promise()
     .then( (result) => { return result.Items } )
