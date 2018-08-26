c1350f27f9e0f120e72f92eef7322517d60e33d3
clicube <clicube@gmail.com>
2018-08-27 03:37:36 +0900

    wip

--- web/store/events.js
+++ web/store/events.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import firebase from '~/plugins/firebase'
 import { firebaseMutations, firebaseAction } from 'vuexfire'
-const db = firebase.database()
-const eventsRef = db.ref('events')
+const db = firebase.firestore()
+db.settings({timestampsInSnapshots: true})
+const eventsRef = db.collection('events')
 const initPlugin = store => store.dispatch('INIT_EVENTS')
 
 Vue.use(Vuex)
